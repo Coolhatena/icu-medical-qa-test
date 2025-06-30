8f9624dd8ef8f97e0bc0f6b6ea7348c49d450d62
Coolhatena <ingsc.geffroy@gmail.com>
2025-06-29 21:45:03 -0700

    Add detection for the plugs

--- catheter-checker.py
+++ catheter-checker.py
@@ -1,29 +1,111 @@
 import cv2
+import os
+import math
 import numpy as np
-from time import sleep
 
-img_name = "img1.jpg"
-q_unicode = ord('q') 
+LOW = np.array([95, 0, 0])
+UPP = np.array([180, 255, 255])
+img_name = "img5.jpg"
+q_unicode = ord('q')
+
+template1_dir = "images/template1"
+template2_dir = "images/template2"
+rotations = [0, 45, 90, 135, 180, 225, 270, 315]
+matching_threshold = 0.5
+
+def rotarGrad(originx, originy, angle, rad):
+	return (round(originx+rad*math.cos(-math.radians(angle))), round(originy+rad*math.sin(-math.radians(angle))))
+
+
+def hsv_filter(img):
+	hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+	msk = cv2.inRange(hsv, LOW, UPP)
+	return msk
+
+
+def detect_best_template(scene_mask, template_dir):
+	best_val = -1
+	best_angle = None
+	best_top_left = None
+	best_w, best_h = 0, 0
+	for angle in rotations:
+		path = os.path.join(template_dir, f"template_{angle}.png")
+		template = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+		if template is None:
+			continue
+
+		res = cv2.matchTemplate(scene_mask, template, cv2.TM_CCOEFF_NORMED)
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+		if max_val > best_val:
+			best_val = max_val
+			best_angle = angle
+			best_top_left = max_loc
+			best_w, best_h = template.shape[::-1]
+
+	print(f"Best val: {best_val}")
+	return {
+		"score": best_val,
+		"angle": best_angle,
+		"top_left": best_top_left,
+		"w": best_w,
+		"h": best_h
+	} if best_val > matching_threshold else None
+
 
 while True:
-	src = cv2.imread("images/" + img_name)
+	img_scene_color = cv2.imread("images/" + img_name)
+	if img_scene_color is None:
+		break
 
-	LOW = np.array([0, 0, 0])
-	UPP = np.array([32, 255, 83])
+	print("###### Test init ######")	
+	img_scene_mask = hsv_filter(img_scene_color)
+	h_scene, w_scene = img_scene_mask.shape
 
-	hsv = cv2.cvtColor(src, cv2.COLOR_BGR2HSV)
-	msk = cv2.inRange(hsv, LOW, UPP)
-	filtered = cv2.bitwise_and(src,src, mask= msk)
+	# Paso 1: detectar template1 en toda la imagen
+	result1 = detect_best_template(img_scene_mask, template1_dir)
 
-	cv2.imshow('Frame', msk)
+	if result1:
+		top_left1 = result1["top_left"]
+		w1, h1 = result1["w"], result1["h"]
+		cx1 = top_left1[0] + w1 // 2
+		cy1 = top_left1[1] + h1 // 2
+		region = "abajo" if cy1 < h_scene // 2 else "arriba"
 
-	kernel = np.ones((5, 5), np.uint8)
-	img_dilation = cv2.dilate(msk, kernel, iterations=2)
+		# Dibujar template1
+		bottom_right1 = (top_left1[0] + w1, top_left1[1] + h1)
+		figure_center1 = (int(top_left1[0] + w1/2), int(top_left1[1] + h1/2))
+		cv2.rectangle(img_scene_color, top_left1, bottom_right1, (0, 255, 0), 2)
+		cv2.line(img_scene_color, figure_center1, rotarGrad(figure_center1[0], figure_center1[1], result1["angle"] + 90, 120), (0, 255, 0), 2)
+		cv2.putText(img_scene_color, f"T1 {result1['angle']}°", (top_left1[0], top_left1[1]-10),
+					cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 0), 2)
 
-	cv2.imshow('Dilated', img_dilation)
+		# Paso 2: definir región de búsqueda para template2
+		if region == "abajo":
+			mask_region2 = img_scene_mask[h_scene // 2:, :]  # parte de abajo
+			offset_y = h_scene // 2
+		else:
+			mask_region2 = img_scene_mask[:h_scene // 2, :]  # parte de arriba
+			offset_y = 0
 
-	key = cv2.waitKey(1)
-	if key == q_unicode: # If 'q' is pressed, close program (Its case sensitive)
+		# Paso 3: detectar template2 en región correspondiente
+		result2 = detect_best_template(mask_region2, template2_dir)
+
+		if result2:
+			top_left2 = (result2["top_left"][0], result2["top_left"][1] + offset_y)
+			w2, h2 = result2["w"], result2["h"]
+
+			bottom_right2 = (top_left2[0] + w2, top_left2[1] + h2)
+			figure_center2 = (int(top_left2[0] + w2/2), int(top_left2[1] + h2/2))
+			cv2.rectangle(img_scene_color, top_left2, bottom_right2, (0, 0, 255), 2)
+			cv2.line(img_scene_color, figure_center2, rotarGrad(figure_center2[0], figure_center2[1], result2["angle"] + 90, 120), (0, 255, 0), 2)
+			cv2.putText(img_scene_color, f"T2 {result2['angle']}°", (top_left2[0], top_left2[1]-10),
+						cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+	print("###### Test end ######\n\n\n\n\n\n\n")
+	cv2.imshow("Deteccion", img_scene_color)
+	cv2.imshow("mask", img_scene_mask)
+	if cv2.waitKey(1) == q_unicode:
 		break
 
 cv2.destroyAllWindows()
